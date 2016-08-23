71b2a8a653ebc9bf1a4f2be6591543f114f8f294
Kyle Frankovich <kylefrankovich@gmail.com>
2016-08-23 16:18:39 -0700

    finished chapter 3 (actually 2)

---
+++ chapter_3_basic_data_structures.py
@@ -0,0 +1,480 @@
+__author__ = 'kfranko'
+
+# notes for Chapter 3 - Basic Data Structures
+
+'''
+first up we're implementing a "Stack" abstract data type; ordered collection
+of items where items are added and removed from the end; stacks are ordered LIFO
+
+implementing an abstract data type in object-oriented programming language like
+python is done through the creation of a new class; stack operations are
+implemented as methods; for the stack (ordered collection of elements), we'll
+utilize the power and simplicity of the primitive collections provided by python,
+the list
+'''
+
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+
+
+s=Stack()
+
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
+
+
+# Write a function revstring(mystr) that uses a stack to reverse the characters in a string.
+
+def revString(string):
+    newRevString = ''
+    stringStack = Stack()
+
+    for letter in string:
+        stringStack.push(letter)
+
+    while not stringStack.isEmpty():
+        newRevString = newRevString + stringStack.pop()
+
+    return newRevString
+
+string_to_reverse = 'mathematical'
+
+new_reversed_string = revString(string_to_reverse)
+
+# using stacks to determine if parentheses are balanced
+
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top,symbol):
+                       balanced = False
+        index = index + 1
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+def matches(open,close):
+    opens = "([{"
+    closers = ")]}"
+    return opens.index(open) == closers.index(close)
+
+
+print(parChecker('{{([][])}()}'))
+print(parChecker('[{()]'))
+
+
+# using stacks to convert numbers to decimal:
+
+def divideBy2(decNumber):
+    remstack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % 2
+        remstack.push(rem)
+        decNumber = decNumber // 2
+
+    binString = ""
+    while not remstack.isEmpty():
+        binString = binString + str(remstack.pop())
+
+    return binString
+
+print(divideBy2(42))
+
+
+def baseConverter(decNumber,base):
+    digits = "0123456789ABCDEF"
+
+    remstack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % base
+        remstack.push(rem)
+        decNumber = decNumber // base
+
+    newString = ""
+    while not remstack.isEmpty():
+        newString = newString + digits[remstack.pop()]
+
+    return newString
+
+print(baseConverter(25,2))
+print(baseConverter(25,16))
+print(baseConverter(42,10))
+print(baseConverter(42,2))
+
+print(baseConverter(26,26))
+
+
+'''
+queue abstract data type: again, we create a new class for implementing
+an abstract data type queue; likewise, we again use list collection to build the
+queue
+'''
+
+# note that we're using first item as the rear, last item is the front;
+# to do so we need to use insert, this results in enqueue method will be
+# O(n), while dequeue method will be O(1)
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+# hot potato simulation:
+
+from pythonds.basic.queue import Queue
+
+def hotPotato(namelist, num):
+    simqueue = Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+
+    while simqueue.size() > 1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+
+        simqueue.dequeue()
+
+    return simqueue.dequeue()
+
+print(hotPotato(["Bill","David","Susan","Jane","Kent","Brad"],7))
+
+
+
+# printing task simulation:
+
+
+from pythonds.basic.queue import Queue
+
+import random
+
+class Printer:
+    def __init__(self, ppm):
+        self.pagerate = ppm
+        self.currentTask = None
+        self.timeRemaining = 0
+
+    def tick(self):
+        if self.currentTask != None:
+            self.timeRemaining = self.timeRemaining - 1
+            if self.timeRemaining <= 0:
+                self.currentTask = None
+
+    def busy(self):
+        if self.currentTask != None:
+            return True
+        else:
+            return False
+
+    def startNext(self,newtask):
+        self.currentTask = newtask
+        self.timeRemaining = newtask.getPages() * 60/self.pagerate
+
+class Task:
+    def __init__(self,time):
+        self.timestamp = time
+        self.pages = random.randrange(1,21)
+
+    def getStamp(self):
+        return self.timestamp
+
+    def getPages(self):
+        return self.pages
+
+    def waitTime(self, currenttime):
+        return currenttime - self.timestamp
+
+
+def simulation(numSeconds, pagesPerMinute):
+
+    labprinter = Printer(pagesPerMinute)
+    printQueue = Queue()
+    waitingtimes = []
+
+    for currentSecond in range(numSeconds):
+
+      if newPrintTask():
+         task = Task(currentSecond)
+         printQueue.enqueue(task)
+
+      if (not labprinter.busy()) and (not printQueue.isEmpty()):
+        nexttask = printQueue.dequeue()
+        waitingtimes.append( nexttask.waitTime(currentSecond))
+        labprinter.startNext(nexttask)
+
+      labprinter.tick()
+
+    averageWait=sum(waitingtimes)/len(waitingtimes)
+    print("Average Wait %6.2f secs %3d tasks remaining."%(averageWait,printQueue.size()))
+
+def newPrintTask(numStudents):
+    tasksPerSecond = numStudents
+    num = random.randrange(1,181)
+    if num == 180:
+        return True
+    else:
+        return False
+
+
+
+for i in range(10):
+    simulation(3600,5)
+
+
+'''
+Self Check
+How would you modify the printer simulation to reflect a larger number of students?
+Suppose that the number of students was doubled. You make need to make some reasonable
+assumptions about how this simulation was put together but what would you change?
+Modify the code. Also suppose that the length of the average print task was cut in half.
+Change the code to reflect that change. Finally How would you parametertize the number of students,
+rather than changing the code we would like to make the number of students a parameter of the simulation.
+'''
+
+
+
+'''
+deque abstract data type: again, we create a new class for implementing
+an abstract data type deque; likewise, we again use list collection to build the
+deque
+'''
+
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0,item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+
+# palindrome checker using deque abstract data type:
+
+from pythonds.basic.deque import Deque
+
+def palchecker(aString):
+    chardeque = Deque()
+
+    for ch in aString:
+        chardeque.addRear(ch)
+
+    stillEqual = True
+
+    while chardeque.size() > 1 and stillEqual:
+        first = chardeque.removeFront()
+        last = chardeque.removeRear()
+        if first != last:
+            stillEqual = False
+
+    return stillEqual
+
+print(palchecker("lsdkjfskf"))
+print(palchecker("radar"))
+
+
+# linked lists:
+
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+
+
+
+mylist = UnorderedList()
+
+
+# ordered list abstract data type:
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+
+    def search(self,item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+        return found
+
+    def add(self,item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+
+
+    def isEmpty(self):
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+
+
+mylist = OrderedList()
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+
+print(mylist.size())
+print(mylist.search(93))
+print(mylist.search(100))
+
+
+
+
