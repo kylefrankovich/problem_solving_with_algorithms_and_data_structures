aa325e9b574f77f6b40fc70548cfa69b20d903e7
Kyle Frankovich <kylefrankovich@Kyles-MacBook-Pro.local>
2016-10-21 16:31:37 -0500

    more notes for chapter 4

--- chapter_4_recursion.py
+++ chapter_4_recursion.py
@@ -81,8 +81,113 @@ def palindromecheck(s):
     is_palindrome = True
     while is_palindrome:
 
+def ispalindrome(word):
+    if len(word) < 2: return True
+    if word[0] != word[-1]: return False
+    return ispalindrome(word[1:-1])
 
 
-s = "madam i’m adam!!!!"
+
+# starter code from book:
+
+from test import testEqual
+def removeWhite(s):
+    s = ''.join(e for e in s if e.isalnum())
+    return s
+
+string2 = "madam i’m adam!!!!"
 
 strip_string = ''.join(e for e in s if e.isalnum())
+
+removeWhite(string)
+
+from test import testEqual
+def removeWhite(s):
+    s = ''.join(e for e in s if e.isalnum())
+    return s
+
+def isPal(s):
+    if len(s) < 2: return True
+    if s[0] != s[-1]: return False
+    return isPal(s[1:-1])
+
+testEqual(isPal(removeWhite("x")),True)
+testEqual(isPal(removeWhite("radar")),True)
+testEqual(isPal(removeWhite("hello")),False)
+testEqual(isPal(removeWhite("")),True)
+testEqual(isPal(removeWhite("hannah")),True)
+testEqual(isPal(removeWhite("madam i'm adam")),True)
+
+
+
+## visualizing recursion:
+
+
+# drawing a recursive spiral:
+
+import turtle
+
+myTurtle = turtle.Turtle()
+myWin = turtle.Screen()
+
+def drawSpiral(myTurtle, lineLen):
+    if lineLen > 0:
+        myTurtle.forward(lineLen)
+        myTurtle.right(90)
+        drawSpiral(myTurtle,lineLen-5)
+
+drawSpiral(myTurtle,100)
+myWin.exitonclick()
+
+
+# drawing a recursive tree:
+
+import turtle
+
+from random import randint
+
+randint(15,45)
+
+
+def tree(branchLen,t):
+    if branchLen > 5:
+        t.forward(branchLen)
+        t.right(20)
+        tree(branchLen-15,t)
+        t.left(40)
+        tree(branchLen-15,t)
+        t.right(20)
+        t.backward(branchLen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.color("green")
+    tree(75,t)
+    myWin.exitonclick()
+
+main()
+
+
+'''Self Check
+Modify the recursive tree program using one or all of the following ideas:
+
+Modify the thickness of the branches so that as the branchLen gets smaller, the line gets thinner.
+
+Modify the color of the branches so that as the branchLen gets very short it is colored like a leaf.
+
+Modify the angle used in turning the turtle so that at each branch point the angle is selected at random
+in some range. For example choose the angle between 15 and 45 degrees. Play around to see what looks good.
+    - use a stack? store each random angle, then pop them off to go backward? for some reason can't install
+    any packages in pycharm at the moment...
+
+Modify the branchLen recursively so that instead of always subtracting the same amount you subtract a
+random amount in some range.'''
+
+from pythonds.basic.stack import Stack
+
+rStack = Stack()
