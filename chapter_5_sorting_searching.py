f5ade7250112d612e9db77cb5045b8393aea94c3
Kyle Frankovich <kylefrankovich@Kyles-MacBook-Pro.local>
2016-11-03 19:10:51 -0500

    hash table notes added

--- chapter_5_sorting_searching.py
+++ chapter_5_sorting_searching.py
@@ -92,3 +92,116 @@ def binarySearch(alist, item):
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
 print(binarySearch(testlist, 3))
 print(binarySearch(testlist, 13))
+
+
+## hashing:
+
+
+# a function called hash that takes a string and a table size
+# and returns the hash value in the range from 0 to tablesize-1.
+
+def hash(astring, tablesize):
+    sum = 0
+    for pos in range(len(astring)):
+        sum = sum + ord(astring[pos])
+
+    return sum%tablesize
+
+
+hash('cat',11) # 4
+
+'''It is interesting to note that when using this hash function,
+anagrams will always be given the same hash value. To remedy this,
+we could use the position of the character as a weight. Figure 7 shows
+one possible way to use the positional value as a weighting factor.
+The modification to the hash function is left as an exercise.'''
+
+
+def hashWeighted(astring, tablesize):
+    sum = 0
+    for pos in range(len(astring)):
+        sum = sum + ord(astring[pos])*(pos+1)
+
+    return sum%tablesize
+
+
+ord('cat'[0])*(0+1)
+ord('cat'[1])
+ord('cat'[2])
+
+
+hashWeighted('cat',11) # 3
+
+
+itemsToHash = [113 , 117 , 97 , 100 , 114 , 108 , 116 , 105 , 99]
+
+for e in itemsToHash:
+    print e, e%11
+
+
+# implementing the MAP abstract data type:
+
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+
+def put(self,key,data):
+  hashvalue = self.hashfunction(key,len(self.slots))
+
+  if self.slots[hashvalue] == None:
+    self.slots[hashvalue] = key
+    self.data[hashvalue] = data
+  else:
+    if self.slots[hashvalue] == key:
+      self.data[hashvalue] = data  #replace
+    else:
+      nextslot = self.rehash(hashvalue,len(self.slots))
+      while self.slots[nextslot] != None and \
+                      self.slots[nextslot] != key:
+        nextslot = self.rehash(nextslot,len(self.slots))
+
+      if self.slots[nextslot] == None:
+        self.slots[nextslot]=key
+        self.data[nextslot]=data
+      else:
+        self.data[nextslot] = data #replace
+
+def hashfunction(self,key,size):
+     return key%size
+
+def rehash(self,oldhash,size):
+    return (oldhash+1)%size
+
+
+def get(self,key):
+  startslot = self.hashfunction(key,len(self.slots))
+
+  data = None
+  stop = False
+  found = False
+  position = startslot
+  while self.slots[position] != None and  \
+                       not found and not stop:
+     if self.slots[position] == key:
+       found = True
+       data = self.data[position]
+     else:
+       position=self.rehash(position,len(self.slots))
+       if position == startslot:
+           stop = True
+  return data
+
+def __getitem__(self,key):
+    return self.get(key)
+
+def __setitem__(self,key,data):
+    self.put(key,data)
+
+
+
+
+
+
