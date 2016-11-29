e03c46e717787b08d535d8b6a2f69fcc931f96f8
Kyle Frankovich <kylefrankovich@Kyles-MacBook-Pro.local>
2016-11-28 21:22:20 -0600

    added notes for chapter 6

--- chapter_6_trees_and_tree_algorithms.py
+++ chapter_6_trees_and_tree_algorithms.py
@@ -1,2 +1,271 @@
 # notes for Chapter 6 - Lists and List Algorithms
 
+# nodes and references:
+
+# using object-oriented programming representation
+# (as opposed to the chapter's first example, using lists of lists)
+
+
+# create a root node:
+class BinaryTree:
+    def __init__(self,rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+
+    # add a left child to the tree:
+    def insertLeft(self,newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    # add a right child to the tree:
+    def insertRight(self,newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+
+
+    '''To round out the definition for a simple binary tree data structure,
+    we will write accessor methods (see Listing 7) for the left and right children,
+    as well as the root values. '''
+
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self,obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
+
+
+r = BinaryTree('a')
+print(r.getRootVal())
+print(r.getLeftChild())
+r.insertLeft('b')
+print(r.getLeftChild())
+print(r.getLeftChild().getRootVal())
+r.insertRight('c')
+print(r.getRightChild())
+print(r.getRightChild().getRootVal())
+r.getRightChild().setRootVal('hello')
+print(r.getRightChild().getRootVal())
+
+
+def buildTree():
+    r = BinaryTree('a')
+    r.insertLeft('b')
+    (r.getLeftChild()).insertRight('d')
+    r.insertRight('c')
+    (r.getRightChild()).insertLeft('e')
+    (r.getRightChild()).insertRight('f')
+    return r
+
+ttree = buildTree()
+
+ttree.getRightChild().getRootVal()
+
+r = BinaryTree('a')
+r.insertLeft('b')
+b.insertRight('d')
+(r.getLeftChild().getRootVal()).insertRight('d')
+
+(r.getLeftChild()).insertRight('d')
+
+
+from test import testEqual
+
+testEqual(ttree.getRightChild().getRootVal(),'c')
+testEqual(ttree.getLeftChild().getRightChild().getRootVal(),'d')
+testEqual(ttree.getRightChild().getLeftChild().getRootVal(),'e')
+
+
+
+# 6.6 Parse Tree:
+
+# uses the stack and BinaryTree operations to create a parse tree:
+
+from pythonds.basic.stack import Stack
+from pythonds.trees.binaryTree import BinaryTree
+
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in ['+', '-', '*', '/', ')']:
+            currentTree.setRootVal(int(i))
+            parent = pStack.pop()
+            currentTree = parent
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = pStack.pop()
+        else:
+            raise ValueError
+    return eTree
+
+pt = buildParseTree("( ( 10 + 5 ) * 3 )")
+pt.postorder()  #defined and explained in the next section
+
+
+# example recursive evaluate function:
+
+def evaluate(parseTree):
+    opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC),evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
+
+# 6.7 Tree Traversals
+
+# preorder tree traversal, written as an external
+# function that takes a binary tree as a parameter:
+
+def preorder(tree):
+    if tree:
+        print(tree.getRootVal())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+
+
+# implementing preorder as an internal method
+# of tree data structure itself:
+
+def preorder(self):
+    print(self.key)
+    if self.leftChild:
+        self.leftChild.preorder()
+    if self.rightChild:
+        self.rightChild.preorder()
+
+
+def postorder(tree):
+    if tree != None:
+        postorder(tree.getLeftChild())
+        postorder(tree.getRightChild())
+        print(tree.getRootVal())
+
+
+def inorder(tree):
+  if tree != None:
+      inorder(tree.getLeftChild())
+      print(tree.getRootVal())
+      inorder(tree.getRightChild())
+
+# Notice that in all three of the traversal functions
+# we are simply changing the position of the print statement
+# with respect to the two recursive function calls.
+
+
+'''If we perform a simple inorder traversal of a parse tree we get our
+original expression back, without any parentheses. Let’s modify the basic
+inorder algorithm to allow us to recover the fully parenthesized version of
+the expression. The only modifications we will make to the basic template
+are as follows: print a left parenthesis before the recursive call to the
+left subtree, and print a right parenthesis after the recursive call to the right subtree'''
+
+def printexp(tree):
+  sVal = ""
+  if tree:
+      sVal = '(' + printexp(tree.getLeftChild())
+      sVal = sVal + str(tree.getRootVal())
+      sVal = sVal + printexp(tree.getRightChild())+')'
+  return sVal
+
+
+
+# 6.8 Priority Queues with Binary Heaps
+
+# implementation of a binary heap:
+
+class BinHeap:
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def percUp(self, i):
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i // 2]:
+                tmp = self.heapList[i // 2]
+                self.heapList[i // 2] = self.heapList[i]
+                self.heapList[i] = tmp
+            i = i // 2
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize)
+
+    def percDown(self, i):
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
+
+    def minChild(self, i):
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def delMin(self):
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
+
+    def buildHeap(self, alist):
+        i = len(alist) // 2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist[:]
+        while (i > 0):
+            self.percDown(i)
+            i = i - 1
+
+
+
+
+
+
+
+
+
+
